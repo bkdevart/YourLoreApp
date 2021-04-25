6a2c68e9f0601fc8f700f83b05f1c0b21eee494e
Brandon Knox <bknox.dev@gmail.com>
2021-04-24 17:27:04 -0700

    Initial Commit

---
+++ YourLore/YourLoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourLoreApp.swift
+//  YourLore
+//
+//  Created by Brandon Knox on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct YourLoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
